e4db2aa8a121b1345ec859494630e87c7365d1c8
bui huy bach <bachkame@gmail.com>
2022-06-15 17:37:23 +0700

    fix

--- FE_LAW_FINAL/MainWindow.xaml.cs
+++ FE_LAW_FINAL/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace FE_LAW_FINAL {
     public partial class MainWindow : Window {
         public const string dbcon = @"Data Source=C:\Users\20010844\Desktop\law.db";
         SQLiteConnection conn = new SQLiteConnection(dbcon);
-        SQLiteCommand cmd = new SQLiteCommand();
+        
         SQLiteDataAdapter adapter = new SQLiteDataAdapter();
         public bool isLoaded = false;
         public MainWindow()
@@ -34,14 +34,21 @@ namespace FE_LAW_FINAL {
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string _article = article.Text;
+            int _article = Int32.Parse(article.Text);
             string _articleContent = articleContent.Text;
-            string _clause = clause.Text;
+            int _clause = -1;
+            if(clause.Text != "")
+            {
+                 _clause = Int32.Parse(clause.Text);
+            }
             string _clauseContent = clauseContent.Text;
             string _fineAbove = fineAbove.Text;
             string _fineBelow = fineBelow.Text;
+            string _point = point.Text;
+            string _pointContent = pointContent.Text;
 
-            string query = string.Format("INSERT INTO Law(article, article_content, clause, clause_content, fine_above, fine_below) VALUES('{0}','{1}','{2}','{3}','{4}','{5}')", _article, _articleContent, _clause, _clauseContent, _fineAbove, _fineBelow);
+            string query = string.Format("INSERT INTO Law(article, article_content, clause, clause_content, fine_above, fine_below, point, point_content) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}', '{7}')", _article, _articleContent, _clause, _clauseContent, _fineAbove, _fineBelow, _point, _pointContent);
+            SQLiteCommand cmd = new SQLiteCommand();
             conn.Open();
             cmd.CommandText = query;
             cmd.Connection = conn;
@@ -53,16 +60,16 @@ namespace FE_LAW_FINAL {
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            string _article = article.Text;
+            int _article = Int32.Parse(article.Text);
             string _articleContent = articleContent.Text;
-            string _clause = clause.Text;
+            int _clause = Int32.Parse(clause.Text);
             string _clauseContent = clauseContent.Text;
             string _fineAbove = fineAbove.Text;
             string _fineBelow = fineBelow.Text;
             string query;
 
             query = string.Format("UPDATE Law SET HoTen='{0}', Lop='{1}', TinhTrang='{2}' WHERE id={3}", _article, _articleContent, _clause, _clauseContent);
-
+            SQLiteCommand cmd = new SQLiteCommand();
             cmd.CommandText = query;
             conn.Open();
             cmd.Connection = conn;
@@ -77,6 +84,7 @@ namespace FE_LAW_FINAL {
             string id = ID.Text;
 
             string query = string.Format("DELETE FROM Law WHERE id={0}", id);
+            SQLiteCommand cmd = new SQLiteCommand();
             conn.Open();
             cmd.CommandText = query;
             cmd.Connection = conn;
@@ -89,7 +97,7 @@ namespace FE_LAW_FINAL {
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
             string query = "SELECT * FROM Law";
-
+            SQLiteCommand cmd = new SQLiteCommand();
             conn.Open();
             cmd.CommandText = query;
             cmd.Connection = conn;
@@ -105,7 +113,7 @@ namespace FE_LAW_FINAL {
                 newT.TextWrapping = TextWrapping.WrapWithOverflow;
                 newT.MaxWidth = 1450;
                 newT.MinWidth = 200;
-                newT.Text = String.Format("ID:{0} \nĐiều: {1} \nNội dung điều:\n {2} \nKhoản: {3} \nNội dung khoản: {4} \nMức phạt trên: {5} \nMức phạt dưới: {6}", reader[0], reader[1], reader[2], reader[3], reader[4], reader[5], reader[6]);
+                newT.Text = String.Format("ID:{0} \nĐiều: {1} \nNội dung điều:\n {2} \nKhoản: {3} \nNội dung khoản: {4} \nĐiểm: {5} \nNội dung điểm: {6} \nMức phạt trên: {7} \nMức phạt dưới: {8}", reader[0], reader[1], reader[2], reader[3], reader[4], reader[7], reader[8], reader[5], reader[6]);
                 listBox.Items.Add(newT);
             }
             conn.Close();
